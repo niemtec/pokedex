665f6565dbc623b4f8f3fde06eb9b67ebc452bc7
Jakub Niemiec <niemtec@users.noreply.github.com>
2025-02-01 22:54:44 +0000

    Implemented providers for cubits

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+import 'package:pokedex/data/datasources/pokeapi_datasource.dart';
+import 'package:pokedex/data/repositories/pokemon_data_repository.dart';
+import 'package:pokedex/domain/repositories/pokemon_repository.dart';
+import 'package:pokedex/domain/usecases/get_pokemon_by_name_usecase.dart';
+import 'package:pokedex/domain/usecases/get_pokemon_list_usecase.dart';
+import 'package:pokedex/presentation/bloc/homepage/homepage_cubit.dart';
+import 'presentation/pages/homepage.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +19,33 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiProvider(
+      providers: [
+        Provider<http.Client>(create: (_) => http.Client()),
+        Provider<PokeapiDatasource>(
+          create: (context) => PokeapiDatasourceImpl(context.read<http.Client>()),
+        ),
+        Provider<PokemonRepository>(
+          create: (context) => PokemonDataRepositoryImpl(context.read<PokeapiDatasource>()),
+        ),
+        Provider<GetPokemonListUsecase>(
+          create: (context) => GetPokemonListUsecase(context.read<PokemonRepository>()),
+        ),
+        Provider<GetPokemonByNameUsecase>(
+          create: (context) =>
+              GetPokemonByNameUsecase(pokemonRepository: context.read<PokemonRepository>()),
+        ),
+        BlocProvider<HomepageCubit>(
+          create: (context) => HomepageCubit(
+              getPokemonListUsecase: context.read<GetPokemonListUsecase>(),
+              getPokemonByNameUsecase: context.read<GetPokemonByNameUsecase>()),
+        ),
+      ],
+      child: const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Homepage(),
+          ),
         ),
       ),
     );
