db777d64b42127c5e508509085f28ffd7f65d569
Jakub Niemiec <niemtec@users.noreply.github.com>
2025-02-01 22:55:49 +0000

    Added loading indicator

---
+++ lib/presentation/widgets/loading_indicator.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndicator extends StatefulWidget {
+  const LoadingIndicator({super.key});
+
+  @override
+  State<LoadingIndicator> createState() => _LoadingIndicatorState();
+}
+
+class _LoadingIndicatorState extends State<LoadingIndicator> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RotationTransition(
+        turns: _controller,
+        child: Image.asset(
+          'assets/pokeball-white.png',
+          width: 64,
+          height: 64,
+          color: Colors.red.withOpacity(0.8),
+        ),
+      ),
+    );
+  }
+}
