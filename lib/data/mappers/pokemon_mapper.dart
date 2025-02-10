fc53a4dfccc5e5b2fb805934acdc02775a9caf2f
Jakub Niemiec <niemtec@users.noreply.github.com>
2025-02-10 16:42:33 +0000

    Reduced complexity of the mapper

--- lib/data/mappers/pokemon_mapper.dart
+++ lib/data/mappers/pokemon_mapper.dart
@@ -4,73 +4,56 @@ import 'package:pokeapi_wrapper/pokeapi_wrapper.dart' as api;
 
 class PokemonMapper {
   static Future<List<Pokemon>> toDomain(List<api.Pokemon> pokemonList) async {
-    List<Pokemon> pokemonProfileList = [];
-
-    for (var pokemon in pokemonList) {
-      var types = await _mapPokemonTypes(pokemon.types);
-      pokemonProfileList.add(
-        Pokemon(
+    return Future.wait(
+      pokemonList.map(
+        (pokemon) async => Pokemon(
           id: pokemon.id,
           name: pokemon.name,
           sprite: pokemon.sprites.officialArtWork,
-          types: types,
-          abilities: _mapPokemonAbilities(pokemon.abilities),
+          types: await _mapPokemonTypes(pokemon.types),
+          abilities: await _mapPokemonAbilities(pokemon.abilities),
           species: await _mapSpecies(pokemon.species),
           height: pokemon.height,
           weight: pokemon.weight,
         ),
-      );
-    }
-    return pokemonProfileList;
+      ),
+    );
   }
 }
 
 Future<String> _mapSpecies(Future<api.Either<Error, api.PokemonSpecies>> species) async {
-  String genus = "";
-
-  await species.fold(
-    (error) {
-      return;
-    },
-    (value) {
-      genus = value.genera[7].genus.replaceAll(' Pokémon', '');
-    },
+  return species.then(
+    (result) => result.fold(
+      (error) => throw Exception(error),
+      (value) => value.genera[7].genus.replaceAll(' Pokémon', ''),
+    ),
   );
-  return genus;
 }
 
-List<String> _mapPokemonAbilities(List<api.PokemonAbility> abilities) {
-  List<String> pokemonAbilities = [];
-
-  for (var ability in abilities) {
-    ability.ability.fold(
-      (error) => {
-        throw Exception(error),
+Future<List<String>> _mapPokemonAbilities(List<api.PokemonAbility> abilities) async {
+  return Future.wait(
+    abilities.map(
+      (value) {
+        return value.ability.fold(
+          (error) => throw Exception(error),
+          (ability) => ability.name,
+        );
       },
-      (ability) => {
-        pokemonAbilities.add(ability.name),
-      },
-    );
-  }
-  return pokemonAbilities;
+    ),
+  );
 }
 
 Future<List<PokemonType>> _mapPokemonTypes(List<api.PokemonType> types) async {
-  List<PokemonType> pokemonTypes = [];
-
-  for (var value in types) {
-    await value.type.fold(
-      (error) => {
-        throw Exception(error),
+  return Future.wait(
+    types.map(
+      (value) {
+        return value.type.fold(
+          (error) => throw Exception(error),
+          (type) => Future.value(
+            PokemonType.values.byName(type.name),
+          ), // Wrap in Future
+        );
       },
-      (type) => {
-        pokemonTypes.add(
-          PokemonType.values.byName(type.name),
-          // _getPokemonType(type),
-        ),
-      },
-    );
-  }
-
-  return pokemonTypes;
+    ),
+  );
 }
