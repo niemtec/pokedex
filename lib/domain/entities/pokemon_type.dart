28a58319b7673b2da98900434aa2ecb3af03e937
Jakub Niemiec <niemtec@users.noreply.github.com>
2025-03-14 19:02:47 +0000

    added firebase and icons

--- lib/domain/entities/pokemon_type.dart
+++ lib/domain/entities/pokemon_type.dart
@@ -1,85 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 enum PokemonType {
   normal(
     color: Color(0xFFA8A77A),
-    icon: Icons.person,
+    icon: FontAwesomeIcons.circle,
   ),
   fire(
     color: Color(0xFFFB6C6C),
-    icon: Icons.fitness_center,
+    icon: FontAwesomeIcons.fire,
   ),
   water(
     color: Color(0xFF91CAFD),
-    icon: Icons.airplanemode_active,
+    icon: FontAwesomeIcons.droplet,
   ),
   grass(
     color: Color(0xFF48D0B0),
-    icon: Icons.sick,
+    icon: FontAwesomeIcons.seedling,
   ),
   electric(
     color: Color(0xFFFED86F),
-    icon: Icons.landscape,
+    icon: FontAwesomeIcons.boltLightning,
   ),
   ice(
     color: Color(0xFF96D9D6),
-    icon: Icons.terrain,
+    icon: FontAwesomeIcons.snowflake,
   ),
   fighting(
     color: Color(0xFFC22E28),
-    icon: Icons.bug_report,
+    icon: FontAwesomeIcons.handBackFist,
   ),
   poison(
     color: Color(0xFFA33EA1),
-    icon: Icons.visibility_off,
+    icon: FontAwesomeIcons.skullCrossbones,
   ),
   ground(
     color: Color(0xFFE2BF65),
-    icon: Icons.build,
+    icon: FontAwesomeIcons.circle,
   ),
   flying(
     color: Color(0xFFA98FF3),
-    icon: Icons.local_fire_department,
+    icon: FontAwesomeIcons.fly,
   ),
   psychic(
     color: Color(0xFFF95587),
-    icon: Icons.water_drop,
+    icon: FontAwesomeIcons.brain,
   ),
   bug(
     color: Color(0xFFA6B91A),
-    icon: Icons.grass,
+    icon: FontAwesomeIcons.bug,
   ),
   rock(
     color: Color.fromARGB(255, 164, 162, 152),
-    icon: Icons.flash_on,
+    icon: FontAwesomeIcons.gem,
   ),
   ghost(
     color: Color(0xFF735797),
-    icon: Icons.psychology,
+    icon: FontAwesomeIcons.ghost,
   ),
   dragon(
     color: Color(0xFF6F35FC),
-    icon: Icons.ac_unit,
+    icon: FontAwesomeIcons.dragon,
   ),
   dark(
     color: Color(0xFF705746),
-    icon: Icons.local_fire_department,
+    icon: FontAwesomeIcons.moon,
   ),
   steel(
     color: Color(0xFFB7B7CE),
-    icon: Icons.nightlight_round,
+    icon: FontAwesomeIcons.box,
   ),
   fairy(
     color: Color(0xFFD685AD),
-    icon: Icons.star,
+    icon: FontAwesomeIcons.handSparkles,
   ),
   shadow(
     color: Color(0xFF5A5A5A),
-    icon: Icons.visibility,
+    icon: FontAwesomeIcons.ghost,
   ),
   unknown(
     color: Color(0xFF68A090),
-    icon: Icons.help_outline,
+    icon: FontAwesomeIcons.question,
   );
 
   const PokemonType({
